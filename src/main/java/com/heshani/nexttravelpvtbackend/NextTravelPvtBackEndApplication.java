26e5e2678f812235d06eb0c64820a4a58ee4189d
yehan@2000 <heshanishehara510@gmail.com>
2023-10-18 22:51:40 +0530

    Initial commit

---
+++ src/main/java/com/heshani/nexttravelpvtbackend/NextTravelPvtBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.heshani.nexttravelpvtbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextTravelPvtBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NextTravelPvtBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/heshani/nexttravelpvtbackend/NextTravelPvtBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heshani.nexttravelpvtbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextTravelPvtBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
