6cb227cea611861414841644d59353519300e26b
yehan@2000 <heshanishehara510@gmail.com>
2023-10-19 00:07:07 +0530

    update

--- user_service/src/main/java/com/heshani/us/controller/UserController.java
+++ user_service/src/main/java/com/heshani/us/controller/UserController.java
@@ -1,9 +1,9 @@
 package com.heshani.us.controller;
 
+import org.apache.tomcat.util.http.ResponseUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin("*")
@@ -11,5 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     @Autowired
     private UserService userService;
+
+    @PostMapping(value = "/add_user")
+    public ResponseUtil addUser(@RequestBody User user ){
+        userService.addUser(user);
+
+        return new ResponseUtil("Okay")
+    }
 
 }
