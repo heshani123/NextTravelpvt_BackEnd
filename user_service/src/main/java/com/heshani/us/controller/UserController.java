386975ec29d3cd45b16cbd2820da681b17886bfd
yehan@2000 <heshanishehara510@gmail.com>
2023-10-18 23:54:30 +0530

    update

--- user_service/src/main/java/com/heshani/us/controller/UserController.java
+++ user_service/src/main/java/com/heshani/us/controller/UserController.java
@@ -1,4 +1,15 @@
 package com.heshani.us.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping(value = "/user")
 public class UserController {
+    @Autowired
+    private UserService userService;
+
 }
