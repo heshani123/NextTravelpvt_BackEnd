4d6f15e33e1ea9ae3846fe431a1d43b9a0b75a6a
yehan@2000 <heshanishehara510@gmail.com>
2023-10-18 23:19:54 +0530

    update

---
+++ driver_service/src/test/java/com/heshani/ds/DriverServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heshani.ds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriverServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
