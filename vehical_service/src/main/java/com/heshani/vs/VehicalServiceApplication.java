acd415834593be0460c0b0ecb7dcb3847af3ee32
yehan@2000 <heshanishehara510@gmail.com>
2023-10-18 23:41:29 +0530

    update

---
+++ vehical_service/src/main/java/com/heshani/vs/VehicalServiceApplication.java
@@ -0,0 +1,13 @@
+package com.heshani.vs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicalServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VehicalServiceApplication.class, args);
+    }
+
+}
---
+++ vehical_service/src/test/java/com/heshani/vs/VehicalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heshani.vs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicalServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
