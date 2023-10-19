40f14ea54816c3ebe3c8033150f25f53a966d675
yehan@2000 <heshanishehara510@gmail.com>
2023-10-20 00:45:00 +0530

    update

--- user_service/src/main/java/com/heshani/us/repository/UserRepository.java
+++ user_service/src/main/java/com/heshani/us/repository/UserRepository.java
@@ -7,5 +7,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Integer> {
-    Boolean existsByUserName(String userName);
+    Boolean existsByUsername(String userName);
+    Boolean existsUserByUserNameAndPassword(String userName, String password);
+
 }
