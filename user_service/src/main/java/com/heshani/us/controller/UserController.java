8ec6b29f465e9be3fdcdb6db68af77e24fa19998
yehan@2000 <heshanishehara510@gmail.com>
2023-10-19 23:08:08 +0530

    update

--- user_service/src/main/java/com/heshani/us/controller/UserController.java
+++ user_service/src/main/java/com/heshani/us/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.heshani.us.controller;
 
+import org.apache.catalina.User;
 import org.apache.tomcat.util.http.ResponseUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
@@ -17,6 +18,30 @@ public class UserController {
         userService.addUser(user);
 
         return  new ResponseUtil("Okay","Added",null);
+    }
+    @PutMapping(value = "update_user")
+    public ResponseUtil updateUser(@RequestBody User user){
+        userService.updateUser(user);
+        return new ResponseUtil("Okay","Updated",null);
+    }
+    @DeleteMapping(value = "/delete_user")
+    public ResponseUtill deleteUser(@RequestParam Integer id){
+
+        userService.deleteUser(id);
+        return new ResponseUtil ("Okay","Deleted",null);
+    }
+
+    @GetMapping(value = "/fetch_users")
+    public ResponseUtil fetchAllUsers() {
+        return new ResponseUtill("Okay","Done",userService.fetchAllUsers());
+    }
+
+    @GetMapping(value = "/check")
+    public ResponseUtil checkLogin(@RequestBody loginDto loginDto) {
+        userService.checkLogin(loginDto.getUserName(),loginDto.getPassword());
+        return new ResponseUtil("Okay","Food have",null);
+
+
     }
 
 }
