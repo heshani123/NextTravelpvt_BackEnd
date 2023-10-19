2ca78e3b2825a06063e57d315108933e1683446e
yehan@2000 <heshanishehara510@gmail.com>
2023-10-20 00:31:26 +0530

    update

--- user_service/src/main/java/com/heshani/us/repository/UserRepository.java
+++ user_service/src/main/java/com/heshani/us/repository/UserRepository.java
@@ -1,7 +1,11 @@
 package com.heshani.us.repository;
 
+import org.apache.catalina.User;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository {
+public interface UserRepository extends JpaRepository<User,Integer> {
+
 }
