555232cfa91d390fb69c9dbdfed253a9db845417
yehan@2000 <heshanishehara510@gmail.com>
2023-10-20 13:14:17 +0530

    update

---
+++ user_service/src/main/java/com/heshani/us/util/ResponseUtil.java
@@ -0,0 +1,16 @@
+package com.heshani.us.util;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+public class ResponseUtil {
+
+}
+
