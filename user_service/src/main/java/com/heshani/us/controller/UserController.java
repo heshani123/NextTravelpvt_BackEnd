61919bd2a5dc50ae893dbe6be543070b4b6a7b5c
yehan@2000 <heshanishehara510@gmail.com>
2023-10-28 23:01:40 +0530

    update

--- user_service/src/main/java/com/heshani/us/controller/UserController.java
+++ user_service/src/main/java/com/heshani/us/controller/UserController.java
@@ -2,6 +2,7 @@ package com.heshani.us.controller;
 
 
 import com.heshani.us.service.UserService;
+import com.heshani.us.utill.ResponseUtill;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,5 +13,8 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    public
+    @PostMapping(value = "/add_user")
+    public ResponceUtill addUser (@RequestBody User user){
+        return new ResponseUtill("OK", "Added", userService.addUser(user));
+    }
 }
