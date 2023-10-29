86d6b412088ef31af9352bf601a227d16ac9f331
yehan@2000 <heshanishehara510@gmail.com>
2023-10-30 01:00:52 +0530

    update

---
+++ driver_service/src/main/java/com/heshani/ds/entity/Driver.java
@@ -0,0 +1,18 @@
+package com.heshani.ds.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "driver_details")
+public class Driver {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+}
