d8c9b06a9c97901fcce6979eedfa7a3f490b3872
yehan@2000 <heshanishehara510@gmail.com>
2023-10-30 00:54:30 +0530

    update

--- driver_service/src/main/java/com/heshani/ds/service/DriverService.java
+++ driver_service/src/main/java/com/heshani/ds/service/DriverService.java
@@ -1,4 +1,13 @@
 package com.heshani.ds.service;
 
+import java.util.List;
+
 public interface DriverService {
+    Driver addDriver(Driver driver);
+    List<Driver> fetchAllDriver();
+    boolean checkExistsDriver(Integer driverId,String name);
+    void updateDriver(Driver driver);
+    Driver searchDriver(Integer id);
+    Driver getAvailableDriver();
+    void setUnavailableDriver(Integer id);
 }
