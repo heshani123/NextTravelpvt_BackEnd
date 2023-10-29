9257b65b8974a17b97a8cea6c22b0c97b5b4202e
yehan@2000 <heshanishehara510@gmail.com>
2023-10-30 03:16:17 +0530

    update

--- hotel_service/src/main/java/com/heshani/hs/controller/hotelController.java
+++ hotel_service/src/main/java/com/heshani/hs/controller/hotelController.java
@@ -1,5 +1,6 @@
 package com.heshani.hs.controller;
 
+import com.heshani.hs.entity.Hotel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
--- hotel_service/src/main/java/com/heshani/hs/repository/hotelRepository.java
+++ hotel_service/src/main/java/com/heshani/hs/repository/hotelRepository.java
@@ -2,7 +2,9 @@ package com.heshani.hs.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface hotelRepository extends JpaRepository<Hotel,Integer> {
 
-
+    List<Hotel> findAllByStarRate(Integer starRate);
 }
