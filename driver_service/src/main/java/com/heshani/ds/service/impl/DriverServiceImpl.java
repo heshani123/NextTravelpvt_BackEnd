6a0e18ee06be997bf08ea8ffcd3169e5cd18aa56
yehan@2000 <heshanishehara510@gmail.com>
2023-10-30 01:16:04 +0530

    update

--- driver_service/src/main/java/com/heshani/ds/service/impl/DriverServiceImpl.java
+++ driver_service/src/main/java/com/heshani/ds/service/impl/DriverServiceImpl.java
@@ -1,5 +1,6 @@
 package com.heshani.ds.service.impl;
 
+import com.heshani.ds.repository.DriverRepository;
 import com.heshani.ds.service.DriverService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
