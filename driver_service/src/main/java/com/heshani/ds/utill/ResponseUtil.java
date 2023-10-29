5173a98d0cbd21211dc139b3ee07b25729ca3cfd
yehan@2000 <heshanishehara510@gmail.com>
2023-10-30 02:18:11 +0530

    update

--- driver_service/src/main/java/com/heshani/ds/utill/ResponseUtil.java
+++ driver_service/src/main/java/com/heshani/ds/utill/ResponseUtil.java
@@ -1,4 +1,15 @@
 package com.heshani.ds.utill;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
 public class ResponseUtil {
+    public String status;
 }
