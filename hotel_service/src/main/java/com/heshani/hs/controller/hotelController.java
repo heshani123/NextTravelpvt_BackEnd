1f642f5c167031fb1e435293aff385e15182e104
yehan@2000 <heshanishehara510@gmail.com>
2023-10-30 03:32:04 +0530

    update

--- hotel_service/src/main/java/com/heshani/hs/controller/hotelController.java
+++ hotel_service/src/main/java/com/heshani/hs/controller/hotelController.java
@@ -1,6 +1,7 @@
 package com.heshani.hs.controller;
 
 import com.heshani.hs.entity.Hotel;
+import com.heshani.hs.utill.ResponseUtill;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
