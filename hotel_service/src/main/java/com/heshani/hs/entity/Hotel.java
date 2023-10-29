e00557f9515cd942d4e84e8b4158ca4bd4a280d7
yehan@2000 <heshanishehara510@gmail.com>
2023-10-30 02:50:21 +0530

    update

--- hotel_service/src/main/java/com/heshani/hs/entity/Hotel.java
+++ hotel_service/src/main/java/com/heshani/hs/entity/Hotel.java
@@ -1,4 +1,16 @@
 package com.heshani.hs.entity;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table
 public class Hotel {
 }
