0d9860f6f41e7ef7f1f9101955eb67dda319da41
yehan@2000 <heshanishehara510@gmail.com>
2023-10-31 00:30:25 +0530

    update

--- travel_service/src/main/java/com/heshani/ts/service/Impl/TravelPackageServiceImpl.java
+++ travel_service/src/main/java/com/heshani/ts/service/Impl/TravelPackageServiceImpl.java
@@ -1,6 +1,10 @@
 package com.heshani.ts.service.Impl;
 
+import com.heshani.ts.repository.TravelPackageRepository;
 import com.heshani.ts.service.TravelPackageService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class TravelPackageServiceImpl implements TravelPackageService {
+    @Autowired
+    private TravelPackageRepository travelPackageRepository
 }
