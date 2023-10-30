fb954fd28c50991659babc44f3a7e4a3b37ffa3c
yehan@2000 <heshanishehara510@gmail.com>
2023-10-31 00:12:07 +0530

    update

--- travel_service/src/main/java/com/heshani/ts/entity/TravelPackage.java
+++ travel_service/src/main/java/com/heshani/ts/entity/TravelPackage.java
@@ -1,14 +1,18 @@
 package com.heshani.ts.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "travel_package_details")
 public class TravelPackage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
 }
 
