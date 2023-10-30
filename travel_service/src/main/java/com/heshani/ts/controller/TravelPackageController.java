72a6a99dc3e87fc9810f0d3ae6214c457f41972d
yehan@2000 <heshanishehara510@gmail.com>
2023-10-30 21:41:04 +0530

    update

--- travel_service/src/main/java/com/heshani/ts/controller/TravelPackageController.java
+++ travel_service/src/main/java/com/heshani/ts/controller/TravelPackageController.java
@@ -1,5 +1,40 @@
 package com.heshani.ts.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping(value = "/packages")
 public class TravelPackageController {
+    @Autowired
+    private TravelPackageService travelPackageService;
+
+    @PostMapping(value = "/add_package")
+    public ResponseUtill addTravelPackage(@RequestBody TravelPackage travelPackage) {
+        return new ResponseUtill("Okay", "Added", travelPackageService.addTravelPackage(travelPackage));
+    }
+
+    @PutMapping(value = "/update_package")
+    public ResponseUtill updateTravelPackage(@RequestBody TravelPackage travelPackage) {
+        return new ResponseUtill("Okay", "Done", travelPackageService.updateTravelPackage(travelPackage));
+    }
+
+    @DeleteMapping(value = "/delete_package")
+    public ResponseUtill deletePackage(@RequestParam Integer id) {
+        travelPackageService.deletePackage(id);
+        return new ResponseUtill("Okay", "Deleted", null);
+    }
+
+    @GetMapping(value = "/get_packages")
+    public ResponseUtill fetchAllPackages() {
+        return new ResponseUtill("Okay", "Done", travelPackageService.fetchAllPackages());
+    }
+
+    @GetMapping(value = "/get_package")
+    public ResponseUtill fetchPackageDetails(@RequestParam Integer id) {
+        return new ResponseUtill("Okay", "Done", travelPackageService.fetchPackageDetails(id));
+    }
 
 }
+
