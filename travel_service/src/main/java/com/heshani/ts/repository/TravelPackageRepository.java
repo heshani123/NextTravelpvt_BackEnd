a3a7e8976e91b820cd77ddafc808d8dfc966ef2e
yehan@2000 <heshanishehara510@gmail.com>
2023-10-31 00:20:49 +0530

    update

--- travel_service/src/main/java/com/heshani/ts/repository/TravelPackageRepository.java
+++ travel_service/src/main/java/com/heshani/ts/repository/TravelPackageRepository.java
@@ -1,8 +1,9 @@
 package com.heshani.ts.repository;
 
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
 @Service
-public interface TravelPackageRepository extends  {
+public interface TravelPackageRepository extends JpaRepository<> {
 }
