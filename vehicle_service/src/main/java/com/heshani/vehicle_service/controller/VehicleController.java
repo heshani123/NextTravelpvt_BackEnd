8b9a015ae1b7183af173d8fd636279e31dcf82ea
yehan@2000 <heshanishehara510@gmail.com>
2023-10-31 01:22:33 +0530

    update

--- vehicle_service/src/main/java/com/heshani/vehicle_service/controller/VehicleController.java
+++ vehicle_service/src/main/java/com/heshani/vehicle_service/controller/VehicleController.java
@@ -1,4 +1,12 @@
 package com.heshani.vehicle_service.controller;
 
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping(value = "/vehicle")
 public class VehicleController {
 }
