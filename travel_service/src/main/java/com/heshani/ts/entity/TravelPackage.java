bd39ff5915153da814e2bb2514919b1592e1b624
yehan@2000 <heshanishehara510@gmail.com>
2023-10-31 00:10:10 +0530

    update

--- travel_service/src/main/java/com/heshani/ts/entity/TravelPackage.java
+++ travel_service/src/main/java/com/heshani/ts/entity/TravelPackage.java
@@ -1,5 +1,14 @@
 package com.heshani.ts.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "travel_package_details")
 public class TravelPackage {
 }
 
