4ad36d2a63028766b2ec48eafba55f63717ef995
yehan@2000 <heshanishehara510@gmail.com>
2023-10-31 00:52:38 +0530

    update

--- travel_service/src/main/java/com/heshani/ts/utill/ResponceUtill.java
+++ travel_service/src/main/java/com/heshani/ts/utill/ResponceUtill.java
@@ -1,5 +1,15 @@
 package com.heshani.ts.utill;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
 public class ResponceUtill {
 
 }
