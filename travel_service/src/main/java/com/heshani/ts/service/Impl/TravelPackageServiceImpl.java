744f8f78a3685f13c29979618d6948180ff88300
yehan@2000 <heshanishehara510@gmail.com>
2023-10-31 00:32:48 +0530

    update

--- travel_service/src/main/java/com/heshani/ts/service/Impl/TravelPackageServiceImpl.java
+++ travel_service/src/main/java/com/heshani/ts/service/Impl/TravelPackageServiceImpl.java
@@ -6,5 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 public class TravelPackageServiceImpl implements TravelPackageService {
     @Autowired
-    private TravelPackageRepository travelPackageRepository
+    private TravelPackageRepository travelPackageRepository;
+
+    @Override
+    public TravelPackage addTravelPackage(TravelPackage travelPackage) {
+        if (travelPackageRepository.existsByPackageName(travelPackage.getPackageName())) {
+            throw new RuntimeException("Package Already Exists");
+        }
+        return travelPackageRepository.save(travelPackage);
+    }
 }
